070d6c69dcd0b81842a4be5aada0a683c7c32999
Giuseppe Foti <foti.giuseppe@gmail.com>
2022-03-06 22:50:51 +0100

    added more tests

---
+++ tests/test-Nunil_Lib_Log.php
@@ -0,0 +1,75 @@
+<?php declare(strict_types=1);
+use PHPUnit\Framework\TestCase;
+use NUNIL\Nunil_Lib_Log as Log;
+use NUNIL\Nunil_Lib_Db as Db;
+
+final class Nunil_Lib_LogTest extends WP_UnitTestCase
+{
+	public function tear_down(): void {
+		deactivate_no_unsafe_inline( false );
+		parent::tear_down();
+	}
+	
+    protected function setUp(): void {
+		parent::set_up();
+		activate_no_unsafe_inline( false );
+		
+		// setting log level to debug
+		$options = get_option( 'no-unsafe-inline');
+		$options['log_level'] = 'debug';
+		$options['log_driver'] = 'db';
+		$success = update_option( 'no-unsafe-inline', $options );
+		$this->assertEquals(
+			true, $success
+        );
+        $plugin = new No_Unsafe_Inline;
+        $plugin->load_logger();
+	}
+	
+	public function verify_log_enabled(): void {
+		$options = get_option( 'no-unsafe-inline');
+		$log_level = $options['log_level'];
+		$log_driver = $options['log_driver'];
+		
+		$this->assertEquals(
+			$log_level, 'error'
+        );
+		
+		$this->assertEquals(
+			$log_driver, 'db'
+        );
+	}
+	
+	/**
+     * @dataProvider logsInputArray
+     */
+	public function testLogWriter( string $method, int $level, string $message ) {
+		global $wpdb;
+		
+		
+		
+		Log::$method( $message );
+		
+		$sql = 'SELECT `level`, `message` FROM ' . $wpdb->prefix . 'nunil_logs';
+		$logged = $wpdb->get_results( $sql, OBJECT );
+
+		$obj=new stdClass;
+		$obj->level=$method;
+		$obj->message=$message;
+		
+		$expected = array( $obj );
+		
+		$this->assertEquals(
+			$expected, $logged
+        );
+	}
+	
+	public function logsInputArray(): array {
+		return [
+			'debug'=> [ 'debug', 0, 'This is a debug log' ],
+			'info' => [ 'info', 1, 'This is an info log' ],
+			'warning'=> [ 'warning', 2, 'This is a warning log' ],
+			'error'=> [ 'error', 3, 'This is an error log' ]
+		];
+	}
+}
--- tests/test-Nunil_SRI.php
+++ tests/test-Nunil_SRI.php
@@ -3,23 +3,29 @@ use PHPUnit\Framework\TestCase;
 
 final class Nunil_SRITest extends WP_UnitTestCase {
 
-	public function testisLocalResource(): void {
-		$Nunil_SRI = new NUNIL\Nunil_SRI();
-		$this->assertSame(
-			$Nunil_SRI->isLocalResource( '//fonts.googleapis.com/css?family=Lora%3A400%2C400italic%2C700%2C700italic%7CMontserrat%3A400%2C700%7CMaven+Pro%3A400%2C700&ver=5.9' ),
-			false
+	/**
+	 * @dataProvider getResources
+	 */
+	public function testFetchResource( string $resource ): void {
+		$Nunil_SRI         = new NUNIL\Nunil_SRI();
+		$attended_response = array(
+			'code'    => 200,
+			'message' => 'OK',
 		);
+
+		$response = $Nunil_SRI->fetch_resource( $resource )['response'];
+
 		$this->assertSame(
-			$Nunil_SRI->isLocalResource( 'https://fonts.googleapis.com/css?family=Lora%3A400%2C400italic%2C700%2C700italic%7CMontserrat%3A400%2C700%7CMaven+Pro%3A400%2C700&ver=5.9' ),
-			false
+			$response,
+			$attended_response
 		);
-		$this->assertSame(
-			$Nunil_SRI->isLocalResource( 'http://fonts.googleapis.com/css?family=Lora%3A400%2C400italic%2C700%2C700italic%7CMontserrat%3A400%2C700%7CMaven+Pro%3A400%2C700&ver=5.9' ),
-			false
+	}
+
+	public function getResources(): array {
+		return array(
+			array( '//fonts.googleapis.com/css?family=Lora%3A400%2C400italic%2C700%2C700italic%7CMontserrat%3A400%2C700%7CMaven+Pro%3A400%2C700&ver=5.9' ),
+			array( 'https://fonts.googleapis.com/css?family=Lora%3A400%2C400italic%2C700%2C700italic%7CMontserrat%3A400%2C700%7CMaven+Pro%3A400%2C700&ver=5.9' ),
+			array( 'http://www.example.org' )
 		);
-		//~ $this->assertSame(
-			//~ $Nunil_SRI->isLocalResource( 'wp-includes/js/backbone.js' ),
-			//~ true
-		//~ );
 	}
 }
