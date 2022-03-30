5556f56b7bdd2d01973eb33451a87e9eba9577e0
Giuseppe Foti <foti.giuseppe@gmail.com>
2022-03-30 06:58:11 +0200

    updating dev dependencies

--- Gruntfile.js
+++ Gruntfile.js
@@ -139,7 +139,7 @@ module.exports = function( grunt ) {
 				'public/**/*.js',
 				'includes/**/*.js'
 				]
-			},
+			}
 		},
 		
 		watch: {
@@ -149,7 +149,7 @@ module.exports = function( grunt ) {
 				options: {
 					spawn: false,
 				},
-			},
+			}
 		}
 	} );
 
