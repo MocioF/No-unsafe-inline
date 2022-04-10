9f3e54fbe24aa3ff485663c4641b15d73bb904a2
Giuseppe Foti <foti.giuseppe@gmail.com>
2022-04-10 09:21:03 +0200

    Removing double conversion of nilsimsa, while clustering

--- src/Nunil_Hamming_Distance.php
+++ src/Nunil_Hamming_Distance.php
@@ -23,18 +23,16 @@ use Beager\Nilsimsa;
 class Nunil_Hamming_Distance implements Distance {
 
 	/**
-	 * Calculate distance between 2 Nilsimsa array digests.
+	 * Calculate distance between 2 Nilsimsa hex digests.
 	 *
-	 * @param array<int> $a Nilsimsa array digest.
-	 * @param array<int> $b Nilsimsa array digest.
+	 * @param array<int, string> $a Nilsimsa array digest.
+	 * @param array<int, string> $b Nilsimsa array digest.
 	 *
 	 * @return float
 	 */
 	public function distance( array $a, array $b ) : float {
-		$a_hex = Nunil_Clustering::convertArraytoHexDigest( $a );
-		$b_hex = Nunil_Clustering::convertArraytoHexDigest( $b );
 
-		$similarity = Nilsimsa::compareDigests( $a_hex, $b_hex, $is_hex_1 = true, $is_hex_2 = true );
+		$similarity = Nilsimsa::compareDigests( $a[0], $b[0], $is_hex_1 = true, $is_hex_2 = true );
 
 		$distance = floatval( 128 - $similarity );
 
