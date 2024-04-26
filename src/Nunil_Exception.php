b4e8908ebf53b719e01a5a519f62c514a1d5e8ef
Giuseppe Foti <foti.giuseppe@gmail.com>
2024-04-26 20:07:51 +0200

    Added custom class to manage exception
    Signed-off-by: Giuseppe Foti <foti.giuseppe@gmail.com>

---
+++ src/Nunil_Exception.php
@@ -0,0 +1,121 @@
+<?php
+/**
+ * Exception
+ *
+ * Class used to manage software Exceptions
+ *
+ * @package No_unsafe-inline
+ * @link    https://wordpress.org/plugins/no-unsafe-inline/
+ * @since   1.2.0
+ */
+
+namespace NUNIL;
+
+use NUNIL\Nunil_Lib_Log as Log;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Class used to manage and log software exceptions
+ *
+ * @package No_unsafe-inline
+ * @since   1.2.0
+ */
+class Nunil_Exception extends \Exception {
+
+	public const DEBUG   = 0;
+	public const INFO    = 1;
+	public const WARNING = 2;
+	public const ERROR   = 3;
+
+	/**
+	 * The message to log
+	 *
+	 * @var string
+	 */
+	private string $log_msg;
+
+	/**
+	 * The log level to use
+	 *
+	 * @var int
+	 */
+	private int $log_level;
+
+	/**
+	 * Exception object builder
+	 *
+	 * @param string     $message Exception message.
+	 * @param int        $code Error code.
+	 * @param int        $log_level Log level to use.
+	 * @param \Throwable $previous Previous exception.
+	 */
+	public function __construct( $message = 'Unknown exception', $code = 0, $log_level = 0, \Throwable $previous = null ) {
+		$this->log_level = intval( $log_level );
+		if ( intval( $log_level ) > 3 ) {
+			$this->log_level = 3;
+		}
+		if ( intval( $log_level ) < 0 ) {
+			$this->log_level = 0;
+		}
+		parent::__construct( $message, intval( $code ), $previous );
+	}
+
+	/**
+	 * Build log message
+	 *
+	 * @return void
+	 */
+	private function set_log_msg() {
+		$log_msg = 'Error on line ' . $this->getLine() . ' in ' . $this->getFile() .
+		' - [' . $this->getCode() . ']: ' . $this->getMessage() . '\n' . $this->getTraceAsString();
+		$options = get_option( 'no-unsafe-inline' );
+		if (
+			is_array( $options ) &&
+			array_key_exists( 'log_driver', $options ) &&
+			'db' === $options['log_driver']
+		) {
+			$log_msg       = nl2br( str_replace( NO_UNSAFE_INLINE_PLUGIN_DIR, '[…]', $log_msg ) );
+			$this->log_msg = mb_strimwidth( $log_msg, 0, 1800, ' …', 'utf-8' );
+		} else {
+			$this->log_msg = $log_msg;
+		}
+	}
+
+	/**
+	 * Log excemption message
+	 *
+	 * @return void
+	 */
+	public function logexception() {
+		$this->set_log_msg();
+		switch ( $this->log_level ) {
+			case self::DEBUG:
+				Log::debug( $this->log_msg );
+				break;
+			case self::INFO:
+				Log::info( $this->log_msg );
+				break;
+			case self::WARNING:
+				Log::warning( $this->log_msg );
+				break;
+			case self::ERROR:
+				Log::error( $this->log_msg );
+				break;
+			default:
+				Log::debug( $this->log_msg );
+		}
+	}
+
+	/**
+	 * Custom string representation of object
+	 *
+	 * @return string
+	 */
+	public function __toString() {
+		$this->set_log_msg();
+		return __CLASS__ . ": $this->log_msg\n";
+	}
+}
---
+++ tests/test-Nunil_Exception.php
@@ -0,0 +1,54 @@
+<?php declare(strict_types=1);
+
+use NUNIL\Nunil_Lib_Log as Log;
+use NUNIL\log\Nunil_Lib_Log_Db as DBLog;
+
+final class Nunil_ExceptionTest extends WP_UnitTestCase {
+	
+	public function tear_down(): void {
+		no_unsafe_inline_deactivate( false );
+		parent::tear_down();
+	}
+
+	public function set_up(): void {
+		parent::set_up();
+		no_unsafe_inline_activate( false );
+
+		$plugin = new No_Unsafe_Inline();
+		$plugin->run();
+		$dblog = new DBLog;
+		Log::init( $dblog , 0 );
+	}
+	
+	/**
+	 * @dataProvider raisedExceptions
+	 * @group exceptions
+	 */
+	public function test_logexception( $e, $expected_level, $expected_count) {
+		global $wpdb;
+
+		$customException = new NUNIL\Nunil_Exception( ...$e );
+		$customException->logexception();
+
+		$sql = 'SELECT * FROM `' . $wpdb->prefix . 'nunil_logs`';
+		$res = $wpdb->get_results( $sql, OBJECT );
+
+		$this->assertSame( $expected_count, count( $res ), "db res is:\n" . print_r( $res, true ) );
+
+		$this->assertEquals( $expected_level, $res[0]->level );
+	}
+
+	public static function raisedExceptions() {
+		return array(
+			array( array(), 'debug', 1 ),
+			array( array( 'only message' ), 'debug', 1 ),
+			array( array( 'message and code', 0001 ), 'debug', 1 ),
+			array( array( 'message and code', '0001' ), 'debug', 1 ),
+			array( array( 'message, code and level debug', 0001, 0 ), 'debug', 1 ),
+			array( array( 'message, code and level debug', '0001', 0 ), 'debug', 1 ),
+			array( array( 'message, code and level info', 1001, 1 ), 'info', 1 ),
+			array( array( 'message, code and level warning', 2001, 2 ), 'warning', 1 ),
+			array( array( 'message, code and level error', 3001, 3 ), 'error', 1 ),
+		);
+	}
+}
