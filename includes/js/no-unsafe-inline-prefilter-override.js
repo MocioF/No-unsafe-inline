13bc0743c383c838bf506948727556b1072e57ac
Giuseppe Foti <foti.giuseppe@gmail.com>
2025-11-24 00:44:10 +0100

    Improve the regexp used to find style attribute in html tags
    Signed-off-by: Giuseppe Foti <foti.giuseppe@gmail.com>

--- includes/js/no-unsafe-inline-prefilter-override.js
+++ includes/js/no-unsafe-inline-prefilter-override.js
@@ -3,7 +3,32 @@
  *
  * https://csplite.com/csp/test433/
  */
-jQuery.htmlPrefilter = function( html ) {
-	return ( html + '' ).replace( / style=/gi, ' data-style=' );
-  // return ( html + '' ).replace( / (?<!<script.*)(?!<\/script>) style==/gim, ' data-style=' );
-};
+//jQuery.htmlPrefilter = function( html ) {
+	// return ( html + '' ).replace( / style=/gi, ' data-style=' );
+  // return ( html + '' ).replace( /(?<=<[^>]*?\s)\s*style\s*=\s*/gi, ' data-style=' );
+//};
+
+(function() {
+    // Test per supporto lookbehind
+    let supportsLookbehind = false;
+    try {
+        new RegExp("(?<=a)b"); // Se non lancia errore, lookbehind è supportato
+        supportsLookbehind = true;
+    } catch (e) {
+        supportsLookbehind = false;
+    }
+
+    // Definisci la regex in base al supporto
+    const regex = supportsLookbehind
+        ? /(?<=<[^>]*?\s)\s*style\s*=\s*/gi // Lookbehind: sicuro e preciso
+        : /(<[^>]*?\s)\s*style\s*=\s*/gi;   // Fallback: usa gruppo di cattura
+
+    // Override di jQuery.htmlPrefilter
+    jQuery.htmlPrefilter = function(html) {
+        if (supportsLookbehind) {
+            return (html + '').replace(regex, ' data-style=');
+        } else {
+            return (html + '').replace(regex, '$1data-style=');
+        }
+    };
+})();
